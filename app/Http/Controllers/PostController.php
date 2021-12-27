0715656d8acdb8aa7e7cb34e8764576294f4bab6
hudsonviana <hudson1206@gmail.com>
2021-12-27 17:38:37 -0300

    Creating a post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,9 +16,22 @@ class PostController extends Controller
             'body' => 'required'
         ]);
 
+        $request->user()->posts()->create($request->only('body'));
+
+        // outras formas de criar esse método: 
+            
+        // $request->user()->posts()->create([
+        //     'body' => $request->body
+        // ]);
+
+
         // Post::create([
         //     'user_id' => auth()->id,
         //     'body' => $request->body
         // ]);
+
+        return back();
+
+    
     }
 }
