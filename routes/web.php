94ed9e9c6a4ef818f154088e231c05bc9641d2cb
hudsonviana <hudson1206@gmail.com>
2021-12-26 21:43:43 -0300

    Signing in after registration

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboardName');
-
 Route::get('/register', [RegisterController::class, 'index'])->name('registerName');
 Route::post('/register', [RegisterController::class, 'store']);
 
