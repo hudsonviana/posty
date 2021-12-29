56038980381120e69d89f319ae9068cdc54fb71c
hudsonviana <hudson1206@gmail.com>
2021-12-29 01:35:06 -0300

    Blade components

--- resources/views/users/posts/index.blade.php
+++ resources/views/users/posts/index.blade.php
@@ -2,8 +2,25 @@
 
 @section('content')
     <div class="flex justify-center">
-        <div class="w-8/12 bg-white p-6 rounded-lg">
-            {{ $user->name }}
+        <div class="w-8/12">
+
+            <div class="p-6">
+                <h1 class="text-2xl font-medium mb-1">{{ $user->name }}</h1>
+                <p>Postou {{ $posts->count() }} {{ Str::plural('post', $posts->count()) }}</p>
+            </div>
+    
+            <div class="bg-white p-6 rounded-lg">
+                @if ($posts->count())
+                    @foreach ($posts as $post)
+                        <x-post :post="$post"/>
+                    @endforeach
+    
+                    {{ $posts->links() }} {{-- comando para mostrar as páginas --}}
+                @else
+                    <p>{{ $user->name }} não tem nenhum post.</p>
+                @endif
+            </div>
+
         </div>
     </div>
 @endsection
