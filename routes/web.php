8313b98369d2ccb969e7230bdfc757f7a57c35a4
hudsonviana <hudson1206@gmail.com>
2021-12-29 18:47:08 -0300

    Single post page

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,7 @@ Route::get('/register', [RegisterController::class, 'index'])->name('registerNam
 Route::post('/register', [RegisterController::class, 'store']);
 
 Route::get('/posts', [PostController::class, 'index'])->name('postsName');
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 Route::post('/posts', [PostController::class, 'store']);
 Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('postsName.destroy');
 
